1879af7d39e91b6d2d9ac4c8a46f242435f3c8c6
alex.pilipenko <oleksii.pylypenko@intellias.com>
2025-03-27 18:59:27 +0200

    delete

--- MvcAspAzure/Repository/IRepositoryAsync.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MvcAspAzure.Repository {
-    public interface IRepositoryAsync<T> where T : class {
-        Task<IEnumerable<T>> GetAllAsync();
-        Task<T?> GetByIdAsync(int id);
-        Task<T> InsertAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(int id);
-    }
-}
--- MvcAspAzure/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MvcAspAzure.ViewModels {
-    public sealed class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
